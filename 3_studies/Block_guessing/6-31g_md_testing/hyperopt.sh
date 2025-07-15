19b12ac22b213a94854b5d7fb8085a55e152b693
Elias Wachmann <elias.wachmann@gmail.com>
2025-07-15 16:19:50 +0200

    slurm scripts delete my tmp files before i start a run

--- 3_studies/Block_guessing/6-31g_md_testing/hyperopt.sh
+++ 3_studies/Block_guessing/6-31g_md_testing/hyperopt.sh
@@ -6,13 +6,16 @@
 #SBATCH --partition=local     # use partition that fits to the qos
 #SBATCH --mem=48G
 
+# clean /tmp
+find /tmp -maxdepth 1 -type f -user "$(whoami)" -delete
+
 cd "/home/ewachmann/REPOS/Masterarbeit/3_studies/Block_guessing/6-31g_md_testing"
 
 module purge
 module load python/3.11.9-gcc-13.2.0-eqozuli miniconda3/latest
 eval "$(conda shell.bash hook)"
 conda activate scf_tools_311
-"using python: $( python --version ) from $( which python )"
+echo "using python: $( python --version ) from $( which python )"
 
 export PYSCF_TMPDIR=~/scratch/pyscf_tmp
 mkdir -p $PYSCF_TMPDIR
